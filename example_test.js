ded9ebcdf21a74ad2987491049c01f4e2bdca00e
seta-huongdv <huong.duong@setacinq.vn>
2019-03-13 17:24:45 +0700

    u

--- example_test.js
+++ example_test.js
@@ -1,7 +1,13 @@
 
-Feature('Example test.js');
+Feature('Test Case');
 
-Scenario('test something', (I) => {
+Scenario('test something 1', (I) => {
+    I.amOnPage('https://github.com');
+    I.see('GitHub');
+    I.wait(5);
+});
+
+Scenario('test something 2', (I) => {
     I.amOnPage('https://github.com');
     I.see('GitHub');
     I.wait(5);
